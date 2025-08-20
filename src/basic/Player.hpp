c5779dccd8f571574d0f31d00dfe73b402cc18b2
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2025-08-20 21:46:12 +0000

    gui start

--- src/basic/Player.hpp
+++ src/basic/Player.hpp
@@ -3,10 +3,19 @@
 #include <video/Video.hpp>
 #include <memory>
 #include <string_view>
+#include "imgui.h"
 
 class Player {
 private:
     SDL_Window* window;
+    float main_scale;
+
+    bool show_demo_window = true;
+    bool show_another_window = true;
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    ImGuiIO& io;
 
     uint64_t time{0};
 
@@ -16,6 +25,10 @@ public:
 
     Player(const std::string_view& name);
     ~Player();
+
+    ImGuiIO& InitImGuiContext();
+    void InitImGui();
+    void GuiPass();
     uint64_t DeltaTime();
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 #include "basic/Player.hpp"
-#include <imgui.h>
+#include "backends/imgui_impl_sdl3.h"
 
 SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     Player* player = new Player("video player");
@@ -24,6 +24,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
     SDL_RenderClear(player->renderer);
     player->video->Render();
+    // player->GuiPass();
     SDL_RenderPresent(player->renderer);
 
     uint64_t frameTime = SDL_GetTicksNS() - frameStart;
@@ -36,10 +37,11 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     Player* player = static_cast<Player*>(appstate);
 
+    ImGui_ImplSDL3_ProcessEvent(event);
     switch (event->type) {
     case SDL_EVENT_QUIT:
         return SDL_APP_SUCCESS;
-    case SDL_EVENT_KEY_DOWN:
+    // case SDL_EVENT_KEY_DOWN:
         player->video->Seek(30.0);
 
         break;
